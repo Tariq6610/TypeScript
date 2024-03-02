fa2bf065df837224fa4f7f559c054eb2459815bd
Tariq6610 <115495245+Tariq6610@users.noreply.github.com>
2024-03-02 21:09:40 +0500

    Initial commit

---
+++ project_Q45/src/main1.ts
@@ -0,0 +1,75 @@
+// Q2
+// let Myname : string = "TariqSyed";
+// console.log(`Hello ${Myname}, would you like to learn some python today?`);
+
+
+// Q3
+// let Myname : string = "TariqSyed";
+// console.log(Myname.toLocaleLowerCase());
+// console.log(Myname.toUpperCase());
+
+// let str1 = Myname.slice(0,1).toUpperCase();
+// let str2 = Myname.slice(1).toLowerCase();
+// let titleCase = str1 + str2;
+// console.log(titleCase);
+
+
+// Q4
+// console.log(`imran Khan once said, "Man strives, and success is bestowed by Allah"`);
+
+
+// Q5
+// let message : string = `"Man strives, and success is bestowed by Allah."`;
+// let famous_person : string = "Imran Khan";
+// console.log(`${famous_person} once said, ${message}`);
+
+
+// Q6
+// let Name : string = "\n\tImran Khan\t\n";
+// console.log(Name);
+
+// console.log(Name.trim());
+
+
+// Q7
+// console.log(5+3);
+// console.log(10-2);
+// console.log(4*2);
+// console.log(16/2);
+
+
+// Q8
+// Q7 and Q8 are similar
+
+// Q10 as Question 10 is about writing a comment so it is my comment here I will describe the function of Q9 which is to store my favorite number then display it in the console. 
+// Q9
+// let favoriteNumber : number = 7;
+// console.log(`My Favorite Number is ${favoriteNumber}`)
+
+
+// Q11
+// let Names : string[] = ["Asad","Arsalan","kami","Niaz"];
+// for(let i = 0; i < Names.length; i++){
+//     console.log(Names[i]);
+// }
+
+
+// Q12
+// let Names : string[] = ["Asad","Arsalan","kami","Niaz"];
+// let greetings : string = "Good evening";
+// for (let i = 0; i < Names.length; i++){
+//     console.log(greetings+", "+Names[i]);
+// }
+
+
+// Q13
+// let favorites : string[] = ["suzuki Bike","Audi","Toyota","lexus","BMW"]
+// favorites.forEach(car => {
+//     console.log(`i would like to own a ${car} someday!`);
+// });
+
+
+
+
+
+
---
+++ project_Q45/src/main4.ts
@@ -0,0 +1,169 @@
+// Q33
+// let ordinalNumbers : number[] = [1,2,3,4,5,6,7,8,9];
+// ordinalNumbers.forEach(num => {
+//     if(num == 1){
+//         console.log(`${num}st`)
+//     }else if(num == 2){
+//         console.log(`${num}nd`)   
+//     }else if(num == 3){
+//         console.log(`${num}rd`)
+//     }
+//     else{
+//         console.log(`${num}th`)
+//     }
+// });
+
+
+// Q34
+// let pizzas :string[] = ["Dominos","KFC","Macdonalds"]
+// pizzas.forEach( pizza => {
+//     console.log(pizza);
+// })
+
+// pizzas.forEach( pizza => {
+//     console.log(`i like ${pizza}`);
+// })
+
+// console.log("Domino's offers customizable pizzas with various toppings and crust options, known for its efficient delivery service. With a commitment to quality and convenience, that's why Domino's remains a top choice for me and i love it.")
+
+
+// Q35
+// let animals : string[] = ["Lion","Tiger","Cheetah","leopard"];
+// animals.forEach(cat => {
+//     console.log(cat);
+// });
+
+// animals.forEach(cat => {
+//     if(cat == "Lion"){
+//         console.log('\nIn the wild, the majestic lion commands attention with its regal presence. With its golden mane and powerful stature, the lion symbolizes strength and courage, ruling over the savannah with pride and authority.')
+//     }else if(cat == 'Tiger'){
+//         console.log('\nTigers, on the other hand, embody stealth and grace. With their striking orange coats adorned with bold black stripes, tigers are the epitome of feline elegance. They prowl through dense forests with silent determination, showcasing their unparalleled hunting prowess.')
+//     }else if(cat == 'Cheetah'){
+//         console.log('\nMoving swiftly across the African plains, the cheetah is a marvel of speed and agility. With its sleek, aerodynamic body and distinctive spots, the cheetah is built for acceleration, capable of reaching breathtaking speeds in pursuit of its prey.')
+//     }else if(cat == 'Leopard'){
+//         console.log('\nLastly, the leopard exudes an aura of mystery and intrigue. With its rosette-patterned coat blending seamlessly into the dappled shadows of the jungle, the leopard is a master of camouflage. Stealthy and elusive, it navigates its forested domain with unmatched cunning.')
+//     }
+// });
+
+// console.log("\nDespite their differences in appearance and behavior, lions, tigers, cheetahs, and leopards share several common traits. As apex predators, they occupy the upper echelons of the food chain, playing vital roles in maintaining the delicate balance of their respective ecosystems. Each possesses formidable physical capabilities, including sharp claws, keen senses, and powerful jaws, honed through centuries of evolutionary adaptation. Additionally, these magnificent big cats are revered across cultures for their beauty, strength, and inherent grace, captivating the hearts and imaginations of people around the world.")
+
+
+// Q36
+// function make_shirt(size : string,text : string){
+//     console.log(`A shirt of size ${size} with a text ${text} has been made`)
+// }
+
+// make_shirt('"medium"','"superMan"')
+
+
+
+// Q37
+// function make_shirt2(size : string = "large", text : string = "i love typeScript"): void{
+//         console.log(`A shirt of size ${size} with a text ${text} `)
+     
+// }
+// make_shirt2();
+// make_shirt2("medium")
+// make_shirt2("small","i love python")
+
+
+// Q38
+// function describe_city(city : string, country : string = "Pakistan",): void{
+//         console.log(`${city} is in ${country}`)
+// }
+
+// describe_city("karachi");
+// describe_city("Lahore");
+// describe_city("Peshawar");
+// describe_city("Landon","UK");
+
+
+// Q39
+// function city_country(city : string, country : string){
+//     let cityCountry = city+","+country;
+//     return cityCountry
+// }
+
+// console.log(city_country("karachi","Pakistan"))
+// console.log(city_country("Lahore","Pakistan"))
+// console.log(city_country("Paris","France"))
+
+
+// Q40
+// function make_album(ArtistName : string, AlbumTitle : string): {name : string; title: string}{
+//     return{
+//         name : ArtistName,
+//         title : AlbumTitle
+//     }
+// }
+
+// let Album = make_album("abc","def")
+// console.log(Album);
+
+
+// Q41
+// let magicians = ["magician A","magician B","magician C","magician D"];
+
+// function show_magicians(arr : string[]){
+//     arr.forEach(magician => {
+//         console.log(magician)
+//     });
+// }
+
+// show_magicians(magicians);
+
+
+// // Q42
+// let magicians2 = ["magician A","magician B","magician C","magician D"];
+
+// function make_great(arr : string[]){
+//     let newArr = []
+//     for(let i = 0; i < arr.length; i++){
+//         newArr[i] = "the Great "+arr[i]
+//     }
+//     return newArr;
+// }
+
+// console.log(make_great(magicians2));
+
+
+// // Q43
+// // functions and array has been used from Question 41 and 42
+// let newMagicians2 = make_great(magicians2);
+
+// show_magicians(magicians);
+// show_magicians(newMagicians2);
+
+
+// Q44
+// let items1 :string[] = ["cheese","beef","kethcup","tomato","cucumber",]
+// let items2 :string[] = ["beef","sauce","tomato","cucumber","egg"]
+// let items3 :string[] = ["cheese","beef","kethcup","sauce","tomato","cucumber","egg"]
+
+// function make_sandwiche(arr : string[]){
+//     console.log("The sandwich has been ordered which consist of")
+//     arr.forEach(item => {
+//         console.log(item)
+//     });
+// }
+
+// make_sandwiche(items1);
+// make_sandwiche(items2);
+// make_sandwiche(items3);
+
+
+
+// Q45
+// function Select_car(manuf : string, modl : string, clr : string, fuelTpe : string):{Manufacuturer : string; Model : string; Color : string; FuelType : string}{
+//     return{
+//         Manufacuturer : manuf,
+//         Model : modl,
+//         Color : clr,
+//         FuelType : fuelTpe,
+//     }
+// }
+
+// let car = Select_car("Toyota","Camry","Blue","Hybrid")
+
+// console.log(car)
+
