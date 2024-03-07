c4005181529cdc0be7d461b6b59fcc7c71978f4f
Tariq6610 <115495245+Tariq6610@users.noreply.github.com>
2024-03-07 11:51:21 +0500

    uncomment all files and updated some duplicate variable names

--- project_Q45/src/main2.ts
+++ project_Q45/src/main2.ts
@@ -1,113 +1,113 @@
 // Q14
-// let guests : string[] = ["farhan","waseem","waleed","kamran","sharukh"];
-// guests.forEach(person => {
-//     console.log(`${person}, I would like to invite you for a dinner! plz join us tomorrow`);
-// });
+let guests : string[] = ["farhan","waseem","waleed","kamran","sharukh"];
+guests.forEach(person => {
+    console.log(`${person}, I would like to invite you for a dinner! plz join us tomorrow`);
+});
 
 
 // here i am reusing the variable guest which is declared and defined in Q14
-// // Q15
-// guests = ["farhan","waseem","waleed","kamran","sharukh"];
-// guests.forEach(person => {
-//     // console.log(`${person}, I would like to invite you for a dinner! plz join us tomorrow`);
-// });
+// Q15
+guests = ["farhan","waseem","waleed","kamran","sharukh"];
+guests.forEach(person => {
+    // console.log(`${person}, I would like to invite you for a dinner! plz join us tomorrow`);
+});
 
-// let unarrivedGuest = guests.splice(3,1,"Huzaifa");
-// console.log(`unfortunately ${unarrivedGuest} can not come for the dinner party`);
-// guests.forEach(person => {
-//     console.log(`${person}, I would like to invite you for a dinner! plz join us tomorrow`);
-// });
+let unarrivedGuest = guests.splice(3,1,"Huzaifa");
+console.log(`unfortunately ${unarrivedGuest} can not come for the dinner party`);
+guests.forEach(person => {
+    console.log(`${person}, I would like to invite you for a dinner! plz join us tomorrow`);
+});
 
 
-// // Q16
-// let newGuestList : string[] = ["farhan","waseem","waleed","huzaifa","sharukh"];
-// newGuestList.unshift("Awais");
-// newGuestList.splice(2,0,"Arsalan");
-// newGuestList.push("Asad");
-// console.log("As I found bigges dinner table!")
-// newGuestList.forEach(person => {
-//     console.log(`${person}, I would like to invite you for a dinner! plz join us tomorrow`);
-// });
+// Q16
+let newGuestList : string[] = ["farhan","waseem","waleed","huzaifa","sharukh"];
+newGuestList.unshift("Awais");
+newGuestList.splice(2,0,"Arsalan");
+newGuestList.push("Asad");
+console.log("As I found bigges dinner table!")
+newGuestList.forEach(person => {
+    console.log(`${person}, I would like to invite you for a dinner! plz join us tomorrow`);
+});
 
 
 // here i am reusing the variable newguestList which is declared and defined in Q16
-// // Q17
-// let firstRemoved = newGuestList.pop()
-// console.log(`I am Sorry ${firstRemoved} I can't invite you for a dinner!`)
-// let secondRemoved = newGuestList.pop()
-// console.log(`I am Sorry ${secondRemoved} I can't invite you for a dinner!`)
-// let thirdRemoved = newGuestList.pop()
-// console.log(`I am Sorry ${thirdRemoved} I can't invite you for a dinner!`)
-// let fourthRemoved = newGuestList.pop()
-// console.log(`I am Sorry ${fourthRemoved} I can't invite you for a dinner!`)
-// let fifthRemoved = newGuestList.pop()
-// console.log(`I am Sorry ${fifthRemoved} I can't invite you for a dinner!`)
-// let sixthRemoved = newGuestList.pop()
-// console.log(`I am Sorry ${sixthRemoved} I can't invite you for a dinner!`)
+// Q17
+let firstRemoved = newGuestList.pop()
+console.log(`I am Sorry ${firstRemoved} I can't invite you for a dinner!`)
+let secondRemoved = newGuestList.pop()
+console.log(`I am Sorry ${secondRemoved} I can't invite you for a dinner!`)
+let thirdRemoved = newGuestList.pop()
+console.log(`I am Sorry ${thirdRemoved} I can't invite you for a dinner!`)
+let fourthRemoved = newGuestList.pop()
+console.log(`I am Sorry ${fourthRemoved} I can't invite you for a dinner!`)
+let fifthRemoved = newGuestList.pop()
+console.log(`I am Sorry ${fifthRemoved} I can't invite you for a dinner!`)
+let sixthRemoved = newGuestList.pop()
+console.log(`I am Sorry ${sixthRemoved} I can't invite you for a dinner!`)
 
-// newGuestList.forEach(person => {
-//     console.log(`${person}, I would like to let you know that you are still invited for the dinner!`);
-// });
+newGuestList.forEach(person => {
+    console.log(`${person}, I would like to let you know that you are still invited for the dinner!`);
+});
 
-// newGuestList.pop()
-// newGuestList.pop()
+newGuestList.pop()
+newGuestList.pop()
 
-// console.log(newGuestList);
+console.log(newGuestList);
 
 
 // Q18
-// let favPlaces : string[] = ["Landon","NYC","Istanbul","Madina","Warsaw","Melbourne"];
-// console.log("origional array : ")
-// console.log(favPlaces);
+let favPlaces : string[] = ["Landon","NYC","Istanbul","Madina","Warsaw","Melbourne"];
+console.log("origional array : ")
+console.log(favPlaces);
 
-// //sorted array, first got copy of origional array in order to keep original array intact
-// console.log("sorted array : ")
-// let sortedfavPlaces = favPlaces.slice();
-// console.log(sortedfavPlaces.sort());
+//sorted array, first got copy of origional array in order to keep original array intact
+console.log("sorted array : ")
+let sortedfavPlaces = favPlaces.slice();
+console.log(sortedfavPlaces.sort());
 
-// //to check original array
-// console.log("origional array : ")
-// console.log(favPlaces);
+//to check original array
+console.log("origional array : ")
+console.log(favPlaces);
 
-// //Reverse sorted array, first got copy of origional array in order to keep original array intact
-// console.log("Reverse sorted array")
-// let reverseFavPlaces = favPlaces.slice();
-// console.log(reverseFavPlaces.sort((a,b) => b.localeCompare(a)));
+//Reverse sorted array, first got copy of origional array in order to keep original array intact
+console.log("Reverse sorted array")
+let reverseFavPlaces = favPlaces.slice();
+console.log(reverseFavPlaces.sort((a,b) => b.localeCompare(a)));
 
-// //to check original array
-// console.log("origional array : ")
-// console.log(favPlaces);
+//to check original array
+console.log("origional array : ")
+console.log(favPlaces);
 
-// //Reversed the original array
-// console.log("Reversed the Original array")
-// console.log(favPlaces.reverse());
+//Reversed the original array
+console.log("Reversed the Original array")
+console.log(favPlaces.reverse());
 
-// //Reversed back the original array
-// console.log("Reversed the Original array")
-// console.log(favPlaces.reverse());
+//Reversed back the original array
+console.log("Reversed the Original array")
+console.log(favPlaces.reverse());
 
 
-// //sorted the original array ascending
-// console.log("original array sorted ascending")
-// console.log(favPlaces.sort());
+//sorted the original array ascending
+console.log("original array sorted ascending")
+console.log(favPlaces.sort());
 
-// //sorted the original ascending order array to descending order
-// console.log("the original ascending sorted array has reversed to descending")
-// console.log(favPlaces.reverse());
+//sorted the original ascending order array to descending order
+console.log("the original ascending sorted array has reversed to descending")
+console.log(favPlaces.reverse());
 
 
 // Q19
-//used node.length method to print the length of the array
-// let MyGuestList : string[] = ["farhan","waseem","waleed","huzaifa","sharukh"];
-// console.log(`I am inviting ${MyGuestList.length} people to dinner`)
+// used node.length method to print the length of the array
+let MyGuestList : string[] = ["farhan","waseem","waleed","huzaifa","sharukh"];
+console.log(`I am inviting ${MyGuestList.length} people to dinner`)
 
 
 // Q20
-// let countries : string[] = ["Pakistan","India","China","Afghanistan","Iran"];
-// let i = 1
-// countries.forEach(country => {
-//     console.log(`${i}- ${country}`)
-//     i++
-// });
+let countries : string[] = ["Pakistan","India","China","Afghanistan","Iran"];
+let i = 1
+countries.forEach(country => {
+    console.log(`${i}- ${country}`)
+    i++
+});
 
 
--- project_Q45/src/main3.ts
+++ project_Q45/src/main3.ts
@@ -1,218 +1,218 @@
 // Q21
-// let Myself = {
-//     Name : "Tariq",
-//     Age : 23,
-//     'Highest Qualification' : "bachelors",
-//     Batch : "1_wed-9To12"
-// };
-// console.log(Myself);
+let Myself = {
+    Name : "Tariq",
+    Age : 23,
+    'Highest Qualification' : "bachelors",
+    Batch : "1_wed-9To12"
+};
+console.log(Myself);
 
 
 // Q22
-// let IndexError : number[] = [1,2,3,4];
-// let ind = 5;
-// if(ind < IndexError.length){
-//     console.log(IndexError[ind]);
-// }else{
-//     console.log("index is out of bounds")
-// }
+let IndexError : number[] = [1,2,3,4];
+let ind = 5;
+if(ind < IndexError.length){
+    console.log(IndexError[ind]);
+}else{
+    console.log("index is out of bounds")
+}
 
 
 // Q23
-// let country : string = "Pakistan";
-// console.log("is country == 'Pakistan'? i predict True");
-// console.log(country == "Pakistan");
+let country : string = "Pakistan";
+console.log("is country == 'Pakistan'? i predict True");
+console.log(country == "Pakistan");
 
-// let iAm : string = "student"
-// console.log(" iAm == 'student'? i predict True");
-// console.log(iAm == "student");
-// let city = "karachi";
-// console.log("is city == 'karachi'? i predict True");
-// console.log(city == "karachi");
-// let continent : string = "Asia";
-// console.log("is continent == 'Asia'? i predict True");
-// console.log(continent == "Asia");
-// let galaxy : string = "Milkyway";
-// console.log("is galaxy == 'milkyway'? i predict True");
-// console.log(galaxy == "Milkyway");
-// let Earth : string = "oval";
-// console.log("is Earth == 'flat'? i predict false");
-// console.log(Earth == "flat");
-// let largestContinent : string = "Asia";
-// console.log("is largesContinent == 'Europe'? i predict False")
-// console.log(largestContinent == "Europe");
-// let neighbour : string = "China";
-// console.log("is Neighbour == 'Bangladesh'? i predict False");
-// console.log(neighbour == "Bangladesh");
-// let hardestObject : string = "Diamond";
-// console.log("is hardestObject == 'Steel'? i predict False");
-// console.log(hardestObject == "Steel");
-// let grass : string = "Green";
-// console.log("is grass == 'Blue'? i predict False");
-// console.log(grass == "Blue");
+let iAm : string = "student"
+console.log(" iAm == 'student'? i predict True");
+console.log(iAm == "student");
+let city = "karachi";
+console.log("is city == 'karachi'? i predict True");
+console.log(city == "karachi");
+let continent : string = "Asia";
+console.log("is continent == 'Asia'? i predict True");
+console.log(continent == "Asia");
+let galaxy : string = "Milkyway";
+console.log("is galaxy == 'milkyway'? i predict True");
+console.log(galaxy == "Milkyway");
+let Earth : string = "oval";
+console.log("is Earth == 'flat'? i predict false");
+console.log(Earth == "flat");
+let largestContinent : string = "Asia";
+console.log("is largesContinent == 'Europe'? i predict False")
+console.log(largestContinent == "Europe");
+let neighbour : string = "China";
+console.log("is Neighbour == 'Bangladesh'? i predict False");
+console.log(neighbour == "Bangladesh");
+let hardestObject : string = "Diamond";
+console.log("is hardestObject == 'Steel'? i predict False");
+console.log(hardestObject == "Steel");
+let grass : string = "Green";
+console.log("is grass == 'Blue'? i predict False");
+console.log(grass == "Blue");
 
 
 
 // Q24
-// let str1 : string = "my name is M.tariq";
-// let str2 : string = "My name is not traiq syed";
-// console.log(str1 == str2)
+let stri1 : string = "my name is M.tariq";
+let stri2 : string = "My name is not traiq syed";
+console.log(stri1 == stri2)
 
-// let str3 : string = "i live in karachi";
-// let str4 : string = "i live in karachi";
-// console.log(str3 == str4);
+let str3 : string = "i live in karachi";
+let str4 : string = "i live in karachi";
+console.log(str3 == str4);
 
-// let str5 : string = str2.toLowerCase()
-// console.log(str2 == str5)
+let str5 : string = stri2.toLowerCase()
+console.log(stri2 == str5)
 
-// console.log(str5 == str2.toLowerCase());
+console.log(str5 == stri2.toLowerCase());
 
-// let x = 25;
-// let y = 30;
-// console.log(x > 20);
-// console.log(x < 25);
-// console.log(x != y);
-// console.log(y == 30);
-
-
-// console.log(x == 25 && y != 30);
-// console.log(x == 25 || y != 30);
+let x = 25;
+let y = 30;
+console.log(x > 20);
+console.log(x < 25);
+console.log(x != y);
+console.log(y == 30);
 
 
-// let nums : number[] = [1,2,3,5,6,7]
+console.log(x == 25 && y != 30);
+console.log(x == 25 || y != 30);
 
-// let check4 = false;
-// nums.forEach(num => {
-//     if(num == 4){
-//         console.log("4 is present in this array")
-//         check4 = true;
-//     }
-// })
 
-// if(check4 == false){
-//     console.log(" 4 is not present in this array")
-// }
+let nums : number[] = [1,2,3,5,6,7]
 
-// if (nums.includes(3)){
-//     console.log("3 is present in this array")
-// }else{
-//     console.log("3 is not present in this array")
-// }
+let check4 = false;
+nums.forEach(num => {
+    if(num == 4){
+        console.log("4 is present in this array")
+        check4 = true;
+    }
+})
 
-// if (nums.includes(4)){
-//     console.log("4 is present in this array")
-// }else{
-//     console.log("4 is not present in this array")
-// }
+if(check4 == false){
+    console.log(" 4 is not present in this array")
+}
+
+if (nums.includes(3)){
+    console.log("3 is present in this array")
+}else{
+    console.log("3 is not present in this array")
+}
+
+if (nums.includes(4)){
+    console.log("4 is present in this array")
+}else{
+    console.log("4 is not present in this array")
+}
 
 // Q25
-// let alien_color : string = "green"
-// if(alien_color == "green"){
-//     console.log("the player earned 5 points");
-// }else if(alien_color !== "green"){
-//         console.log("the player earned 10 points");  
-// }
+let alien_color : string = "green"
+if(alien_color == "green"){
+    console.log("the player earned 5 points");
+}else if(alien_color !== "green"){
+        console.log("the player earned 10 points");  
+}
 
 
 
-// // Q26
-// let alien_color2 : string = "blue";
-// if(alien_color2 == "green"){
-//     console.log("the player earned 5 points")
-// }else{
-//     console.log("the player just earned 10 points")
-// }
+// Q26
+let alien_color2 : string = "blue";
+if(alien_color2 == "green"){
+    console.log("the player earned 5 points")
+}else{
+    console.log("the player just earned 10 points")
+}
 
 
 // Q27
-// let alien_color3 : string = "yellow";
-// if(alien_color3 == "green"){
-//     console.log("the player just earned 5 points")
-// }else if(alien_color3 == "yellow"){
-//     console.log("the player just earned 10 points")
-// }else if(alien_color3 == "red"){
-//     console.log("the player just earned 15 points")
-// }
+let alien_color3 : string = "yellow";
+if(alien_color3 == "green"){
+    console.log("the player just earned 5 points")
+}else if(alien_color3 == "yellow"){
+    console.log("the player just earned 10 points")
+}else if(alien_color3 == "red"){
+    console.log("the player just earned 15 points")
+}
 
 
 // Q28
-// let age : number = 23;
-// if(age < 2){
-//     console.log("the person is a baby")
-// }else if(age >= 4 && age < 13){
-//     console.log("the person is a kid")
-// }else if(age >= 13 && age < 20){
-//     console.log("the person is a teenager")
-// }else if(age >= 20 && age < 65){
-//     console.log("the person is an adult")
-// }else if(age >= 65){
-//     console.log("the person is an elder")
-// }
+let age : number = 23;
+if(age < 2){
+    console.log("the person is a baby")
+}else if(age >= 4 && age < 13){
+    console.log("the person is a kid")
+}else if(age >= 13 && age < 20){
+    console.log("the person is a teenager")
+}else if(age >= 20 && age < 65){
+    console.log("the person is an adult")
+}else if(age >= 65){
+    console.log("the person is an elder")
+}
 
 // Q29
-// let favFruits : string[] = ["grape","peach","pomegranate",]
+let favFruits : string[] = ["grape","peach","pomegranate",]
 
-// if(favFruits.includes("grape")){
-//     console.log("i really like grapes")
-// }
+if(favFruits.includes("grape")){
+    console.log("i really like grapes")
+}
 
-// if(favFruits.includes("apple")){
-//     console.log("i really like apple")
-// }
+if(favFruits.includes("apple")){
+    console.log("i really like apple")
+}
 
-// if(favFruits.includes("peach")){
-//     console.log("i really like peach")
-// }
+if(favFruits.includes("peach")){
+    console.log("i really like peach")
+}
 
-// if(favFruits.includes("pomegranate")){
-//     console.log("i really like pomegranate")
-// }
+if(favFruits.includes("pomegranate")){
+    console.log("i really like pomegranate")
+}
 
-// if(favFruits.includes("pineapple")){
-//     console.log("i really like pineapple")
-// }
+if(favFruits.includes("pineapple")){
+    console.log("i really like pineapple")
+}
 
 
 // Q30
-// let userNames : string[] = ["Admin","Tariq","Asad","Waleed","Arsalan","Waseem"];
-// userNames.forEach(person => {
-//     if(person == "Admin"){
-//         console.log(`Hello admin, would you like to see a status report?`)
-//     }else{
-//         console.log(`Hello ${person}, thank you for logging in again.`)
-//     }
-// }) 
+let userNames : string[] = ["Admin","Tariq","Asad","Waleed","Arsalan","Waseem"];
+userNames.forEach(person => {
+    if(person == "Admin"){
+        console.log(`Hello admin, would you like to see a status report?`)
+    }else{
+        console.log(`Hello ${person}, thank you for logging in again.`)
+    }
+}) 
 
 
 // Q31
-// let userNames2 : string[] = [];
-// if(userNames2.length !== 0){
-//     userNames2.forEach(person => {
-//         if(person == "Admin"){
-//             console.log(`Hello admin, would you like to see a status report?`)
-//         }else{
-//             console.log(`Hello ${person}, thank you for logging in again.`)
-//         }
-//     }) 
-// }else{
-//     console.log("we need do find some users first!")
-// }
+let userNames2 : string[] = [];
+if(userNames2.length !== 0){
+    userNames2.forEach(person => {
+        if(person == "Admin"){
+            console.log(`Hello admin, would you like to see a status report?`)
+        }else{
+            console.log(`Hello ${person}, thank you for logging in again.`)
+        }
+    }) 
+}else{
+    console.log("we need do find some users first!")
+}
 
 
 // Q32
-// let currentUsers : string[] = ["Admin","Tariq","Waseem"];
-// let NewUsers : string[] = ["Admin","Tariq","Asad","Waleed","Arsalan","Waseem"];
-// let flag = false;
-// for(let i = 0; i < NewUsers.length; i++){
-//     for(let j = 0; j < currentUsers.length; j++){
-//         if(NewUsers[i] == currentUsers[j]){
-//             console.log("The name is already in the list Please enter a new User name")
-//             flag = true;
-//             break;
-//         }
-//     }
-//     if(flag == false){
-//         console.log(`The user name ${NewUsers[i]} is availble`)
-//     }
-//     flag = false;
-// }
+let currentUsers : string[] = ["Admin","Tariq","Waseem"];
+let NewUsers : string[] = ["Admin","Tariq","Asad","Waleed","Arsalan","Waseem"];
+let flag = false;
+for(let i = 0; i < NewUsers.length; i++){
+    for(let j = 0; j < currentUsers.length; j++){
+        if(NewUsers[i] == currentUsers[j]){
+            console.log("The name is already in the list Please enter a new User name")
+            flag = true;
+            break;
+        }
+    }
+    if(flag == false){
+        console.log(`The user name ${NewUsers[i]} is availble`)
+    }
+    flag = false;
+}
