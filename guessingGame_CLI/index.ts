91b8d46097196cac60998104b60a3f4124b9ab0a
Tariq6610 <115495245+Tariq6610@users.noreply.github.com>
2024-04-22 07:09:33 +0500

    updated

--- guessingGame_CLI/index.ts
+++ guessingGame_CLI/index.ts
@@ -1,53 +1,73 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-import select from "inquirer"
-import chalk from "chalk"
+import select from "inquirer";
+import chalk from "chalk";
 
-let GetRange = await select.prompt([{message: "select the range between 1 to", type: "list", name:"selectRange", choices:["5","10","15"]}])
-let range : number = 5 
-if(GetRange.selectRange == 5){
-    range = 5 
-}else if(GetRange.selectRange == 10){
-    range = 10 
-}else if(GetRange.selectRange == 15){
-    range = 15 
+let GetRange = await select.prompt([
+  {
+    message: "select the range between 1 to",
+    type: "list",
+    name: "selectRange",
+    choices: ["5", "10", "15"],
+  },
+]);
+let range: number = 5;
+if (GetRange.selectRange == 5) {
+  range = 5;
+} else if (GetRange.selectRange == 10) {
+  range = 10;
+} else if (GetRange.selectRange == 15) {
+  range = 15;
 }
 
-// let max: number = 5;
-// let min: number = 1;
 
 let answer;
-let flag : boolean = false;
+let flag: boolean = false;
 let attempts = 5;
 let score = 5;
 
-for(let i = 0; i < attempts; i++){
-    const randomFloat = Math.random() * range + 1;
-    const randomInt = Math.floor(randomFloat);
-       answer = await inquirer.prompt([
-      { message: `guess the number between 1 to ${range} =>`, type: "number", name: "guess" },
-    ]);
+for (let i = 0; i < attempts; i++) {
+  const randomFloat = Math.random() * range + 1;
+  const randomInt = Math.floor(randomFloat);
+  answer = await inquirer.prompt([
+    {
+      message: `guess the number between 1 to ${range} =>`,
+      type: "number",
+      name: "guess",
+    },
+  ]);
 
-    if(answer.guess === randomInt){
-        console.log(chalk.whiteBright.bgGreen.bold("Hurray! You guessed the Right Number"));
-        flag = true;
-        break;
-    }else if(answer !== randomInt && i < 4){
-        console.log(chalk.bgRedBright.white.bold(`You guessed the wrong Number. the Right number was \t`)+chalk.green.underline(randomInt) +`\n`+ chalk.underline.italic.bold.white.bgBlue.underline("Please Try Again"))
-        score--;
-    }
-    
-
-}   
-if(flag == false){
-    console.log(chalk.bgRed.white.bold("You haven't got Right any of the Number. Better Luck Next Time"));
+  if (answer.guess === randomInt) {
+    console.log(
+      chalk.whiteBright.bgGreen.bold("Hurray! You guessed the Right Number")
+    );
+    flag = true;
+    break;
+  } else if (answer !== randomInt && i < 4) {
+    console.log(
+      chalk.bgRedBright.white.bold(
+        `You guessed the wrong Number. the Right number was \t`
+      ) +
+        chalk.green.underline(randomInt) +
+        `\n` +
+        chalk.underline.italic.bold.white.bgBlue.underline("Please Try Again")
+    );
     score--;
+  }
+}
+if (flag == false) {
+  console.log(
+    chalk.bgRed.white.bold(
+      "You haven't got Right any of the Number. Better Luck Next Time"
+    )
+  );
+  score--;
 }
 
-let Perc = (score/5)*100;
+let Perc = (score / 5) * 100;
 
-console.log(`\n${chalk.yellow.bold(`Your probability of guessing the right number was ${Perc}%`)}`);
-
-
-
-
+console.log(
+  `\n${chalk.yellow.bold(
+    `Your probability of guessing the right number was ${Perc}%`
+  )}`
+);
